357407111264eafab41243b8105706e4b941267a
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-04-21 17:52:59 -0600

    separate gl window / globe resources into structs

--- src/main.rs
+++ src/main.rs
@@ -58,36 +58,13 @@ impl Mouse {
     }
 }
 
-struct Globe {
+struct GlWindow {
     pub ctx: ContextWrapper<PossiblyCurrent, Window>,
     pub event_loop: EventLoop<()>,
-    pub data: Vec<f32>,
-    pub program: Program,
-    pub buffer: Buffer,
-    pub mvp: MvpMatrices,
-    pub mouse: Mouse,
 }
 
-impl Globe {
+impl GlWindow {
     pub fn new(width: f64, height: f64) -> Self {
-        let (ctx, event_loop) = Globe::init_ctx(width, height);
-        let (program, buffer, data, mvp) = Globe::init_resources((width / height) as f32);
-        let mouse = Mouse::new();
-        Self {
-            ctx,
-            event_loop,
-            program,
-            buffer,
-            data,
-            mvp,
-            mouse,
-        }
-    }
-
-    fn init_ctx(
-        width: f64,
-        height: f64,
-    ) -> (ContextWrapper<PossiblyCurrent, Window>, EventLoop<()>) {
         let window = WindowBuilder::new()
             .with_inner_size(LogicalSize::new(width, height))
             .with_title("window");
@@ -106,28 +83,89 @@ impl Globe {
             gl::load_with(|ptr| ctx.get_proc_address(ptr) as *const _);
             gl::Enable(gl::DEPTH_TEST);
         }
-        (ctx, event_loop)
+        Self { ctx, event_loop }
     }
+}
 
-    fn init_resources(aspect: f32) -> (Program, Buffer, Vec<f32>, MvpMatrices) {
+struct Globe {
+    pub data: Vec<f32>,
+    pub program: Program,
+    pub buffer: Buffer,
+}
+
+impl Globe {
+    pub fn new() -> Self {
         let data = get_icosphere(4);
         let buffer = Buffer::new(&data, gl::DYNAMIC_DRAW);
         let program =
             Program::new_from_files("./shaders/vert.glsl", "./shaders/frag.glsl").unwrap();
-        let mvp = MvpMatrices {
+        Self {
+            program,
+            buffer,
+            data,
+        }
+    }
+
+    fn get_draw() -> impl FnMut(&mut Globe) {
+        let mut buf_change = 1.0;
+        let mut buf_change_dir = 1.0;
+        move |globe: &mut Globe| {
+            if !(0.5..1.0).contains(&buf_change) {
+                buf_change_dir = -buf_change_dir;
+            }
+            buf_change += buf_change_dir * 0.001;
+            let data: Vec<f32> = globe.data.iter().map(|x| x * buf_change).collect();
+            globe.buffer.set_data(&data);
+            unsafe {
+                gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+                gl::DrawArrays(gl::TRIANGLES, 0, (data.len() / 3) as i32);
+            }
+        }
+    }
+}
+
+impl Drop for Globe {
+    fn drop(&self) {
+        self.program.drop();
+        self.buffer.drop();
+    }
+}
+
+struct Vis {
+    pub gl_window: GlWindow,
+    pub globe: Globe,
+    pub mvp: MvpMatrices,
+    pub mouse: Mouse,
+}
+
+impl Vis {
+    pub fn new(width: f64, height: f64) -> Self {
+        let gl_window = GlWindow::new(width, height);
+        let globe = Globe::new();
+        let mvp = Vis::init_mvp((width / height) as f32, &globe.program);
+        let mouse = Mouse::new();
+        Self {
+            gl_window,
+            globe,
+            mvp,
+            mouse,
+        }
+    }
+
+    fn init_mvp(aspect: f32, program: &Program) -> MvpMatrices {
+        MvpMatrices {
             proj: UniformMatrix::new(
-                &program,
+                program,
                 "projMatrix",
                 Mat4::perspective_rh_gl(70.0 * std::f32::consts::PI / 180.0, aspect, 0.01, 10.0),
             ),
             view: UniformMatrix::new(
-                &program,
+                program,
                 "viewMatrix",
                 Mat4::look_at_rh(Vec3::new(0.0, 0.0, 2.0), Vec3::ZERO, Vec3::Y),
             ),
-            model: UniformMatrix::new(&program, "modelMatrix", Mat4::IDENTITY),
-        };
-        (program, buffer, data, mvp)
+            model: UniformMatrix::new(program, "modelMatrix", Mat4::IDENTITY),
+        }
     }
 
     fn mouse_move(mouse: &mut Mouse, matrix: &mut UniformMatrix, position: PhysicalPosition<f64>) {
@@ -158,66 +196,51 @@ impl Globe {
         matrix.apply();
     }
 
-    fn get_draw() -> impl FnMut(&mut Vec<f32>, &mut Buffer) {
-        let mut buf_change = 1.0;
-        let mut buf_change_dir = 1.0;
-        move |data: &mut Vec<f32>, buffer: &mut Buffer| {
-            if !(0.5..1.0).contains(&buf_change) {
-                buf_change_dir = -buf_change_dir;
-            }
-            buf_change += buf_change_dir * 0.001;
-            let data: Vec<f32> = data.iter().map(|x| x * buf_change).collect();
-            buffer.set_data(&data);
-            unsafe {
-                gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-                gl::DrawArrays(gl::TRIANGLES, 0, (data.len() / 3) as i32);
-            }
-        }
-    }
-
     pub fn run(mut self) {
-        self.program.bind();
-        self.buffer.bind();
-        set_attrib(&self.program, "position", 3, 3, 0).unwrap();
+        self.globe.program.bind();
+        self.globe.buffer.bind();
+        set_attrib(&self.globe.program, "position", 3, 3, 0).unwrap();
 
         self.mvp.proj.apply();
         self.mvp.view.apply();
         self.mvp.model.apply();
 
-        let mut draw = Globe::get_draw();
+        let mut globe_draw = Globe::get_draw();
 
-        self.event_loop.run(move |event, _, control_flow| {
-            *control_flow = ControlFlow::Wait;
-            match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::CursorMoved { position, .. } => {
-                        Globe::mouse_move(&mut self.mouse, &mut self.mvp.model, position);
+        self.gl_window
+            .event_loop
+            .run(move |event, _, control_flow| {
+                *control_flow = ControlFlow::Wait;
+                match event {
+                    Event::WindowEvent { event, .. } => match event {
+                        WindowEvent::CursorMoved { position, .. } => {
+                            Vis::mouse_move(&mut self.mouse, &mut self.mvp.model, position);
+                        }
+                        WindowEvent::MouseWheel { delta, .. } => {
+                            Vis::mouse_wheel(&mut self.mvp.view, delta);
+                        }
+                        WindowEvent::MouseInput { button, state, .. } => {
+                            Vis::mouse_input(&mut self.mouse, button, state);
+                        }
+                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                        _ => (),
+                    },
+                    Event::LoopDestroyed => {
+                        self.globe.drop();
                     }
-                    WindowEvent::MouseWheel { delta, .. } => {
-                        Globe::mouse_wheel(&mut self.mvp.view, delta);
+                    Event::RedrawRequested(_) => {
+                        globe_draw(&mut self.globe);
+
+                        self.gl_window.ctx.swap_buffers().unwrap();
+                        self.gl_window.ctx.window().request_redraw();
                     }
-                    WindowEvent::MouseInput { button, state, .. } => {
-                        Globe::mouse_input(&mut self.mouse, button, state);
-                    }
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                     _ => (),
-                },
-                Event::LoopDestroyed => {
-                    self.program.drop();
-                    self.buffer.drop();
                 }
-                Event::RedrawRequested(_) => {
-                    draw(&mut self.data, &mut self.buffer);
-                    self.ctx.swap_buffers().unwrap();
-                    self.ctx.window().request_redraw();
-                }
-                _ => (),
-            }
-        });
+            });
     }
 }
 
 fn main() {
-    let globe = Globe::new(500.0, 500.0);
+    let globe = Vis::new(500.0, 500.0);
     globe.run();
 }
