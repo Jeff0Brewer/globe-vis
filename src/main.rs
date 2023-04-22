24ed7cadf7f26f1fa5faa632f005a3c7fdc189f2
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-04-21 20:01:23 -0600

    move run to vis window

--- src/main.rs
+++ src/main.rs
@@ -77,35 +77,6 @@ impl Mouse {
     }
 }
 
-struct GlWindow {
-    pub ctx: ContextWrapper<PossiblyCurrent, Window>,
-    pub event_loop: EventLoop<()>,
-}
-
-impl GlWindow {
-    pub fn new(width: f64, height: f64) -> Self {
-        let window = WindowBuilder::new()
-            .with_inner_size(LogicalSize::new(width, height))
-            .with_title("window");
-        let event_loop = EventLoop::new();
-        let ctx_builder = ContextBuilder::new()
-            .with_gl(GlRequest::GlThenGles {
-                // version 2.0 for WebGL compatibility
-                opengl_version: (2, 0),
-                opengles_version: (2, 0),
-            })
-            .with_multisampling(4)
-            .build_windowed(window, &event_loop);
-        let ctx;
-        unsafe {
-            ctx = ctx_builder.unwrap().make_current().unwrap();
-            gl::load_with(|ptr| ctx.get_proc_address(ptr) as *const _);
-            gl::Enable(gl::DEPTH_TEST);
-        }
-        Self { ctx, event_loop }
-    }
-}
-
 struct Globe {
     pub data: Vec<f32>,
     pub program: Program,
@@ -149,16 +120,77 @@ impl Drop for Globe {
     }
 }
 
-struct Vis {
-    pub gl_window: GlWindow,
+struct VisWindow {
+    pub ctx: ContextWrapper<PossiblyCurrent, Window>,
+    pub event_loop: EventLoop<()>,
+}
+
+impl VisWindow {
+    pub fn new(width: f64, height: f64) -> Self {
+        let window = WindowBuilder::new()
+            .with_inner_size(LogicalSize::new(width, height))
+            .with_title("window");
+        let event_loop = EventLoop::new();
+        let ctx_builder = ContextBuilder::new()
+            .with_gl(GlRequest::GlThenGles {
+                // version 2.0 for WebGL compatibility
+                opengl_version: (2, 0),
+                opengles_version: (2, 0),
+            })
+            .with_multisampling(4)
+            .build_windowed(window, &event_loop);
+        let ctx;
+        unsafe {
+            ctx = ctx_builder.unwrap().make_current().unwrap();
+            gl::load_with(|ptr| ctx.get_proc_address(ptr) as *const _);
+            gl::Enable(gl::DEPTH_TEST);
+        }
+        Self { ctx, event_loop }
+    }
+
+    pub fn run(window: VisWindow, mut vis: VisGl) {
+        vis.setup();
+
+        let mut draw = VisGl::get_draw();
+
+        window.event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CursorMoved { position, .. } => {
+                        vis.mouse_move(position);
+                    }
+                    WindowEvent::MouseWheel { delta, .. } => {
+                        vis.mouse_wheel(delta);
+                    }
+                    WindowEvent::MouseInput { button, state, .. } => {
+                        vis.mouse_input(button, state);
+                    }
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => (),
+                },
+                Event::LoopDestroyed => {
+                    vis.drop();
+                }
+                Event::RedrawRequested(_) => {
+                    draw(&mut vis);
+                    window.ctx.swap_buffers().unwrap();
+                    window.ctx.window().request_redraw();
+                }
+                _ => (),
+            }
+        });
+    }
+}
+
+struct VisGl {
     pub globe: Globe,
     pub mvp: MvpMatrices,
     pub mouse: Mouse,
 }
 
-impl Vis {
+impl VisGl {
     pub fn new(width: f64, height: f64) -> Self {
-        let gl_window = GlWindow::new(width, height);
         let globe = Globe::new();
         let mvp = MvpMatrices::new(
             &globe.program,
@@ -167,43 +199,51 @@ impl Vis {
             Vec3::new(0.0, 0.0, 2.0),
         );
         let mouse = Mouse::new();
-        Self {
-            gl_window,
-            globe,
-            mvp,
-            mouse,
-        }
+        Self { globe, mvp, mouse }
     }
 
-    fn mouse_move(mouse: &mut Mouse, matrix: &mut UniformMatrix, position: PhysicalPosition<f64>) {
-        if mouse.dragging {
-            matrix.data =
-                rotate_from_mouse(matrix.data, position.x - mouse.x, position.y - mouse.y);
-            matrix.apply();
+    fn mouse_move(&mut self, position: PhysicalPosition<f64>) {
+        if self.mouse.dragging {
+            self.mvp.model.data = rotate_from_mouse(
+                self.mvp.model.data,
+                position.x - self.mouse.x,
+                position.y - self.mouse.y,
+            );
+            self.mvp.model.apply();
         }
-        mouse.x = position.x;
-        mouse.y = position.y;
+        self.mouse.x = position.x;
+        self.mouse.y = position.y;
     }
 
-    fn mouse_input(mouse: &mut Mouse, button: MouseButton, state: ElementState) {
+    fn mouse_input(&mut self, button: MouseButton, state: ElementState) {
         if let MouseButton::Left = button {
-            mouse.dragging = match state {
+            self.mouse.dragging = match state {
                 ElementState::Pressed => true,
                 ElementState::Released => false,
             }
         }
     }
 
-    fn mouse_wheel(matrix: &mut UniformMatrix, delta: MouseScrollDelta) {
+    fn mouse_wheel(&mut self, delta: MouseScrollDelta) {
         let ds = match delta {
             MouseScrollDelta::PixelDelta(position) => position.y,
             MouseScrollDelta::LineDelta(_, y) => y as f64,
         };
-        matrix.data = zoom_from_scroll(matrix.data, ds);
-        matrix.apply();
+        self.mvp.view.data = zoom_from_scroll(self.mvp.view.data, ds);
+        self.mvp.view.apply();
     }
 
-    pub fn run(mut self) {
+    pub fn get_draw() -> impl FnMut(&mut VisGl) {
+        let mut globe_draw = Globe::get_draw();
+        move |vis: &mut VisGl| {
+            unsafe {
+                gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+            }
+            globe_draw(&mut vis.globe)
+        }
+    }
+
+    pub fn setup(&self) {
         self.globe.program.bind();
         self.globe.buffer.bind();
         set_attrib(&self.globe.program, "position", 3, 3, 0).unwrap();
@@ -211,45 +251,21 @@ impl Vis {
         self.mvp.proj.apply();
         self.mvp.view.apply();
         self.mvp.model.apply();
-
-        let mut globe_draw = Globe::get_draw();
-
-        self.gl_window
-            .event_loop
-            .run(move |event, _, control_flow| {
-                *control_flow = ControlFlow::Wait;
-                match event {
-                    Event::WindowEvent { event, .. } => match event {
-                        WindowEvent::CursorMoved { position, .. } => {
-                            Vis::mouse_move(&mut self.mouse, &mut self.mvp.model, position);
-                        }
-                        WindowEvent::MouseWheel { delta, .. } => {
-                            Vis::mouse_wheel(&mut self.mvp.view, delta);
-                        }
-                        WindowEvent::MouseInput { button, state, .. } => {
-                            Vis::mouse_input(&mut self.mouse, button, state);
-                        }
-                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                        _ => (),
-                    },
-                    Event::LoopDestroyed => {
-                        self.globe.drop();
-                    }
-                    Event::RedrawRequested(_) => {
-                        unsafe {
-                            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-                        }
-                        globe_draw(&mut self.globe);
-                        self.gl_window.ctx.swap_buffers().unwrap();
-                        self.gl_window.ctx.window().request_redraw();
-                    }
-                    _ => (),
-                }
-            });
     }
 }
 
+impl Drop for VisGl {
+    fn drop(&self) {
+        self.globe.drop();
+    }
+}
+
+fn run_visualization(width: f64, height: f64) {
+    let window = VisWindow::new(width, height);
+    let gl = VisGl::new(width, height);
+    VisWindow::run(window, gl);
+}
+
 fn main() {
-    let globe = Vis::new(500.0, 500.0);
-    globe.run();
+    run_visualization(500.0, 500.0);
 }
