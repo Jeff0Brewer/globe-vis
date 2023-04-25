7ec74e1f3a0fbdeb9e27806277f83acaccd2b3c7
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-04-25 09:58:25 -0600

    rename console panic hook import

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[cfg(target_arch = "wasm32")]
+use console_error_panic_hook::set_once as set_console_panic_hook;
 mod gl_wrap;
 mod globe;
 mod icosphere;
@@ -6,10 +8,9 @@ mod vis;
 use vis::Vis;
 
 fn main() {
+    // set panic hook for browser error logging
     #[cfg(target_arch = "wasm32")]
-    use console_error_panic_hook::set_once as set_panic_hook;
-    #[cfg(target_arch = "wasm32")]
-    set_panic_hook();
+    set_console_panic_hook();
 
     let vis = Vis::new(500.0, 500.0).unwrap();
     Vis::start(vis).unwrap();
